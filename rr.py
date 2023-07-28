efe40726b07dc8cd3b3b797fb687f100b9295be4
Alessandro Minoli <minoli.alessandro@gmail.com>
2023-07-28 14:56:19 +0200

    updated code

--- rr.py
+++ rr.py
@@ -1,10 +1,11 @@
 import numpy as np
+from sklearn.base import BaseEstimator, RegressorMixin
 
-class RidgeRegression:
+class RidgeRegression(BaseEstimator, RegressorMixin):
 
     def __init__(self, λ = 1.0):
         self.λ = λ
-        self.w = None
+        self.w_ = None
 
     def fit(self, X, y):
         
@@ -14,17 +15,14 @@ class RidgeRegression:
         I = np.identity(X.shape[1])
         I[0][0] = 0                         # justify this line
 
-        self.w = np.linalg.inv(X.T @ X + self.λ * I) @ X.T @ y
+        self.w_ = np.linalg.inv(X.T @ X + self.λ * I) @ X.T @ y
 
     def predict(self, X):
 
-        if self.w is None:
+        if self.w_ is None:
             raise RuntimeError('Model is still to fit')
         
         # insert dummy feature
         X = np.insert(X, 0, np.ones(X.shape[0]), axis=1) 
 
-        return X @ self.w
-    
-    def get_params(self, deep=True):
-        return {"λ": self.λ}
+        return X @ self.w_
